eaa6611ceac92691139ceb2964652b7acef151e3
shetty-nithin <shettynithin744@gmail.com>
2022-09-23 22:26:56 +0530

    creating a conflict

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -3,7 +3,7 @@ const {movieMiddleware} = require("../middlewares/index");
 const {authJwt} = require("../middlewares/index");
 
 module.exports = (app) => {
-    app.post("/mba/api/v1/movies",[authJwt.verifyToken, authJwt.isAdmin, movieMiddleware.isValidReleaseStatus, movieMiddleware.isValidGenre], movieController.createMovie);
+    app.post("/mba/api//movies",[authJwt.verifyToken, authJwt.isAdmin, movieMiddleware.isValidReleaseStatus, movieMiddleware.isValidGenre], movieController.createMovie);
     app.get("/mba/api/v1/movies", [authJwt.verifyToken], movieController.getAllMovies);
     app.get("/mba/api/v1/movies/:id", [authJwt.verifyToken, movieMiddleware.isValidParamsId], movieController.getMovieById);
     app.put("/mba/api/v1/movies/:id", [authJwt.verifyToken, authJwt.isAdmin, movieMiddleware.isValidParamsId, movieMiddleware.isValidReleaseStatus, movieMiddleware.isValidGenre], movieController.updateMovie);
